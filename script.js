da02c9bb8029c3d413fa4367f502015ab7d64833
MaxKenny2003 <133268134+MaxKenny2003@users.noreply.github.com>
2025-09-10 11:29:44 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+const words = ["An Aerospace Engineer", "a Computer Engineer", "a Software Engineer", "a U-M Engineer", "eager to learn", "a classical guitar lover"];
+let i = 0, j = 0;
+let isDeleting = false;
+const speed = 150;
+
+function type() {
+  const textEl = document.getElementById("text");
+  const word = words[i];
+
+  if (!isDeleting) {
+    textEl.textContent = word.substring(0, j+1);
+    j++;
+    if (j === word.length) {
+      isDeleting = true;
+      setTimeout(type, 1000);
+      return;
+    }
+  } else {
+    textEl.textContent = word.substring(0, j-1);
+    j--;
+    if (j === 0) {
+      isDeleting = false;
+      i = (i+1) % words.length;
+    }
+  }
+  setTimeout(type, isDeleting ? speed/2 : speed);
+}
+
+type();
